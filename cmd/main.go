e233108d9b9e393ed33c20c8c41fb42fbe610314
Sergey Zhuravlev <zhuravlev.srv@gmail.com>
2025-03-29 14:33:17 +0300

    the commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+
+	theLog := log.New(os.Stdout, "Server: ", log.LstdFlags)
+
+	theServer := server.NewServer(theLog)
+
+	theLog.Printf("Starting server the server on http://localhost:8080/")
+	if err := theServer.HttpServer.ListenAndServe(); err != nil {
+		theLog.Fatalf("Failed to start server: %v", err)
+	}
 
 }
